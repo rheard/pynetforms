e9ce9878892eda67d3e2b5e33c1cbfd0c909cc00
Ryan Heard <ryanwheard@gmail.com>
2020-05-22 02:23:13 -0500

    Call EnableVisualStyles and set some default (i'm tired), because thats what M$ does.

--- winforms/__init__.py
+++ winforms/__init__.py
@@ -4,5 +4,11 @@ from .forms import Form
 from clr import System
 
 
+# TODO: As applications become more complex, we might have ppl start calling these items themselves.
+#   If that needs to be done, break them off into simlar methods here.
+System.Windows.Forms.Application.EnableVisualStyles()
+System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(False)
+
+
 def run(form_output):
     return System.Windows.Forms.Application.Run(form_output)
